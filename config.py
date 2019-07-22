0d1fa429e4eb0c9168d96fd2d0b2b3a479a74963
Gunnar Reiske <reiskegw@gmail.com>
2019-07-22 10:39:01 -0400

    Added/Generated Data

---
+++ config.py
@@ -0,0 +1,6 @@
+stanford_bidirectional_tagger_model_path = "./parserData/stanford-postagger/models/english-bidirectional-distsim.tagger"
+stanford_postagger_jar_path = "./parserData/stanford-postagger/stanford-postagger-3.6.0.jar"
+train_folder = "./parserData/trainData/"
+output_folder = "./parserData/parserOutput/"
+dev_folder = ""
+corenlpserverurl = 'http://localhost:9000'
--- scix_*/config.py
+++
@@ -1,2 +0,0 @@
-stanford_bidirectional_tagger_path = "/Users/gunnar/Documents/ProvingGround/semeval2017-test/crfsemeval/stanford-postagger-full-2015-12-09/models/english-bidirectional-distsim.tagger"
-stanford_postagger_jar_path = "/Users/gunnar/Documents/ProvingGround/semeval2017-test/crfsemeval/stanford-postagger-full-2015-12-09/stanford-postagger-3.6.0.jar"
