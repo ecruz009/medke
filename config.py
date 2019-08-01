05098ba1a8e14f864de4750cdb0c140951e497f7
Gunnar Reiske <reiskegw@gmail.com>
2019-08-01 12:16:39 -0400

    Removed unused stanford-postagger

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-stanford_bidirectional_tagger_path = "./parserData/stanford-postagger/models/english-bidirectional-distsim.tagger"
-stanford_postagger_jar_path = "./parserData/stanford-postagger/stanford-postagger-3.6.0.jar"
 train_folder = "./parserData/trainData/"
 output_folder = "./parserData/parserOutput/"
 dev_folder = ""
